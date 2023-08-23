178ec95902d21eacd4346eaac13d6ce377c61701
Eugen Rochko <eugen@zeonfederated.com>
2023-08-23 22:59:19 +0200

    Change video compression parameters

--- app/models/media_attachment.rb
+++ app/models/media_attachment.rb
@@ -98,16 +98,10 @@ class MediaAttachment < ApplicationRecord
     convert_options: {
       output: {
         'loglevel' => 'fatal',
-        'movflags' => 'faststart',
-        'pix_fmt' => 'yuv420p',
-        'vf' => 'scale=\'trunc(iw/2)*2:trunc(ih/2)*2\'',
-        'vsync' => 'cfr',
+        'preset' => 'veryfast',
         'c:v' => 'h264',
-        'maxrate' => '1300K',
-        'bufsize' => '1300K',
-        'b:v' => '1300K',
-        'frames:v' => 60 * 60 * 3,
-        'crf' => 18,
+        'c:a' => 'aac',
+        'b:a' => '192k',
         'map_metadata' => '-1',
       }.freeze,
     }.freeze,
@@ -135,7 +129,7 @@ class MediaAttachment < ApplicationRecord
       convert_options: {
         output: {
           'loglevel' => 'fatal',
-          :vf => 'scale=\'min(400\, iw):min(400\, ih)\':force_original_aspect_ratio=decrease',
+          :vf => 'scale=\'min(640\, iw):min(640\, ih)\':force_original_aspect_ratio=decrease',
         }.freeze,
       }.freeze,
       format: 'png',
@@ -162,7 +156,7 @@ class MediaAttachment < ApplicationRecord
 
   VIDEO_CONVERTED_STYLES = {
     small: VIDEO_STYLES[:small].freeze,
-    original: VIDEO_FORMAT.freeze,
+    original: VIDEO_FORMAT.deep_merge(convert_options: { 'frames:v' => 10_800 }).freeze, # Limit the total number of frames that can be snuck through a GIF
   }.freeze
 
   THUMBNAIL_STYLES = {
